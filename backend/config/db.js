682e2e634b060346d53ac3114985723c1406fb95
gitderekz <dereklyatuu@gmail.com>
2025-05-01 15:02:43 +0300

    port

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: process.env.DB_PORT||3307,
       dialect: 'mysql',
     }
   );
