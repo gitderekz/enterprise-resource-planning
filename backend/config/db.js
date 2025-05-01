cb69c0a89800c4ef435b67fab0b3c2e90bbc5b86
gitderekz <dereklyatuu@gmail.com>
2025-05-01 14:45:00 +0300

    port

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PORT,
-    process.env.DB_PASSWORD,
     {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: 'mysql',
     }
 );
