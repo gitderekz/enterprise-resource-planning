4da58656164016850948b44f19f1347a119b1792
gitderekz <dereklyatuu@gmail.com>
2025-05-31 22:24:04 +0300

    marekebisho cis

--- frontend/src/app/finance/cis/page.js
+++ frontend/src/app/finance/cis/page.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import Header from '../../components/header';
 import Sidebar from '../../components/sidebar';
+import SchemeDetailModal from './SchemeDetailModal';
 import { useSidebar } from '../../lib/SidebarContext';
 import { useSharedStyles } from '../../sharedStyles';
 import { toast } from 'react-toastify';
